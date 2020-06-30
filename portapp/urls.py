a9ee2ff8a10c811d834617809326702da611509f
Patrick Buzzo <pbuzzo04@gmail.com>
2020-06-30 17:36:51 -0500

    Initial Commit

--- portapp/urls.py
+++ portapp/urls.py
@@ -4,4 +4,8 @@ from portapp import views
 urlpatterns = [
     path('', views.index, name = 'home'),
     path('contact/', views.contact, name = 'contact'),
+    path('skills/', views.skills, name = 'skills'),
+    path('resume/', views.resume, name = 'resume'),
+    path('projects/', views.projects, name = 'projects'),
+    path('certifications/', views.certifications, name = 'certifications'),
 ]
--- portapp/views.py
+++ portapp/views.py
@@ -5,3 +5,15 @@ def index(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+def skills(request):
+    return render(request, 'skills.html')
+
+def resume(request):
+    return render(request, 'resume.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
+def certifications(request):
+    return render(request, 'certifications.html')
