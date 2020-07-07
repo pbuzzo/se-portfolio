b46d01af5a53e615896f2cf148c89483c1df481f
Patrick Buzzo <pbuzzo04@gmail.com>
2020-07-07 12:39:28 -0500

    live site

--- portproject/settings.py
+++ portproject/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,4 +133,4 @@ STATIC_URL = '/static/'
 
 # Configure Django App for Heroku.
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
